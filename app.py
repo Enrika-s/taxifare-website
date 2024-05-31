7025da7e07bea755e60ace4848244e6c90dfa8ef
Enrika-s <158200838+Enrika-s@users.noreply.github.com>
2024-05-31 11:40:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,20 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
 1. Let's ask for:
-- date and timesssss
+- date and time
 - pickup longitude
 - pickup latitude
 - dropoff longitude
 - dropoff latitude
 - passenger count
 '''
+pickup_datetime = st.text_input('Pickup Date and Time', '2014-07-06 19:18:00')
+pickup_longitude = st.number_input('Pickup Longitude', -73.950655)
+pickup_latitude = st.number_input('Pickup Latitude', 40.783282)
+dropoff_longitude = st.number_input('Dropoff Longitude', -73.984365)
+dropoff_latitude = st.number_input('Dropoff Latitude', 40.769802)
+passenger_count = st.number_input('Passenger Count', 1, min_value=1, max_value=8)
+
 
 '''
 ## Once we have these, let's call our API in order to retrieve a prediction
