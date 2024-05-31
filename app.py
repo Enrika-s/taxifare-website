51851f595236b28654bceba8088b698630ffcfe3
Enrika-s <158200838+Enrika-s@users.noreply.github.com>
2024-05-31 12:25:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url('https://t4.ftcdn.net/jpg/04/14/29/73/360_F_414297303_srFo2L1U2cZ4Ano0eQr9Plbf9KYyH0u0.jpg');
+        background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1791677b-5bf5-4fd7-8642-e17627638d62/dcqjiop-ba7d84ab-bb07-45b8-8cba-9f62e6e70fe1.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE3OTE2NzdiLTViZjUtNGZkNy04NjQyLWUxNzYyNzYzOGQ2MlwvZGNxamlvcC1iYTdkODRhYi1iYjA3LTQ1YjgtOGNiYS05ZjYyZTZlNzBmZTEuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pGku0NTCbcUNNAeDM6ki2kUKpkf5AiENiIAGHllzuqo');
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
